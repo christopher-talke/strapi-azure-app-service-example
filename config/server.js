d027f0aee470814590aa5853d448865b3b72861c
Christopher Talke <chris@canopi.com.au>
2021-07-21 21:54:33 +1000

    Kicked of the Strapi project using local MySQL

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bcc12c58e11de3fedc7862ef68dc3f00'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32e0a65f-6acd-48c4-99f5-82c6e72a2133'
+};
